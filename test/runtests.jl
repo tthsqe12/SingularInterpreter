f9ab1aa2d89a044239a9010d2266cca827d1ffc9
Rafael Fourquet <fourquet.rafael@gmail.com>
2019-12-19 13:18:29 +0100

    tests: less brittle loading of test/*.sing files

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,5 @@ include("basics.jl")
 for a in ("assign", "int", "intvec", "intmat", "bigintmat", "list", "poly", "proc")
     SingularInterpreter.reset_runtime()
     println("running ", a, ".sing")
-    execute(read(a*".sing", String))
+    execute(read(joinpath(dirname(@__FILE__), a * ".sing"), String))
 end
