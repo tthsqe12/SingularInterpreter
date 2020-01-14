7466f9349d0d121d60a13f3bec950cf71cdcc901
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-01-14 13:56:35 +0100

    split test/basics.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,15 @@
-using SingularInterpreter
+using Test
 
-include("basics.jl")
+using SingularInterpreter
+using SingularInterpreter: rtcolon, rtequalequal, rtnot, rtand, rtor, SIntVec, SString
 
 for a in ("assign", "int", "intvec", "intmat", "bigintmat", "list", "poly", "proc")
     SingularInterpreter.reset_runtime()
+    jlfile = joinpath(dirname(@__FILE__), a * ".jl")
+    if isfile(jlfile)
+        println("running ", a, ".jl")
+        include(jlfile)
+    end
     println("running ", a, ".sing")
     execute(read(joinpath(dirname(@__FILE__), a * ".sing"), String))
 end
